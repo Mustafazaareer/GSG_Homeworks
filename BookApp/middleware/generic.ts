ed7d121d5c1ee7f4cd1ac38054a4118b1fbd55eb
Mustafa Raed Muflih Zaareer <105263355+Mustafazaareer@users.noreply.github.com>
2023-07-24 20:46:25 +0300

    Add files via upload

--- BookApp/middleware/generic.ts
+++ BookApp/middleware/generic.ts
@@ -1,1 +1,12 @@
+import express from 'express';
 
+const logger = (req:express.Request, res:express.Response, next:express.NextFunction)=>{
+    console.log(`${new Date()} [${req.method} ${req.path}]`);
+    res.locals.logMessage =`${new Date()} [${req.method} ${req.path}]`;
+    console.log(JSON.stringify(req.headers));
+    next();
+}
+
+export {
+    logger
+    }
